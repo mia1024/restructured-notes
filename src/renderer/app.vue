6b797db163238418d1ebcadb23e4e4a80f32daef
Jerie Wang <mail@jerie.wang>
2020-06-15 22:39:28 -0700

    Popped a window

---
+++ src/renderer/app.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="app">
+		{{msg}}
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+	private msg:string = "I got a window!"
+		// data(){
+		// 	return {msg: "Hi"}
+		// }
+}
+
+
+
+</script>
+
+<style type="text/scss">
+	body{
+		text-align: center;
+	}
+</style>
