66501102736ada34e9a5b5e7fa3c5b01b6408142
Jerie Wang <mail@jerie.wang>
2020-06-18 03:42:53 -0700

    Added temporary icon

--- src/main/index.js
+++ src/main/index.js
@@ -1,77 +1,77 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import {app, BrowserWindow} from 'electron'
 import * as path from 'path'
-import { format as formatUrl } from 'url'
+import {format as formatUrl} from 'url'
+
+const os = require("os")
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow
 
+
 function createMainWindow() {
-  const window = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
-    },
-    show: false
-        //frame: false
-  }
-  )
+    const window = new BrowserWindow({
+            webPreferences: {
+                nodeIntegration: true
+            },
+            show: false,
+            //frame: false
+            //icon: path.join(__dirname, "../assets/icon" + (os.platform() === "darwin" ? ".icns" : ".png"))
+        }
+    )
 
-  if (isDevelopment) {
-    window.webContents.openDevTools()
+    if (isDevelopment) {
+        window.webContents.openDevTools()
 
-  }
+    }
 
-  if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-  }
-  else {
-    window.loadURL(formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
-      slashes: true
-    }))
-  }
+    if (isDevelopment) {
+        window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+    } else {
+        window.loadURL(formatUrl({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file',
+            slashes: true
+        }))
+    }
 
-  window.on('closed', () => {
-    mainWindow = null
-  })
-
-  window.once('ready-to-show', () => {
-    window.show()
-  })
-
-  window.webContents.on('devtools-opened', () => {
-    window.focus()
-    setImmediate(() => {
-      window.focus()
+    window.on('closed', () => {
+        mainWindow = null
     })
-    window.webContents.devToolsWebContents.executeJavaScript(
-        'DevToolsAPI.showPanel("chrome-extension://vue-js-devtoolsVue")',
-    );
-  })
 
-  return window
+    window.once('ready-to-show', () => {
+        window.show()
+    })
+
+    window.webContents.on('devtools-opened', () => {
+        window.focus()
+        setImmediate(() => {
+            window.focus()
+        })
+    })
+
+    return window
 }
 
 // quit application when all windows are closed
 app.on('window-all-closed', () => {
-  // on macOS it is common for applications to stay open until the user explicitly quits
-  //if (process.platform !== 'darwin') {
+    // on macOS it is common for applications to stay open until the user explicitly quits
+    //if (process.platform !== 'darwin') {
     app.quit()
-  //}
+    //}
 })
 
 app.on('activate', () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow()
-  }
+    // on macOS it is common to re-create a window even after all windows have been closed
+    if (mainWindow === null) {
+        mainWindow = createMainWindow()
+    }
 })
 
 // create main BrowserWindow when electron is ready
 app.on('ready', () => {
-  mainWindow = createMainWindow()
+    mainWindow = createMainWindow()
 })
