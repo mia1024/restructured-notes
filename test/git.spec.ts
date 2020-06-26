808255f195891c86cf6736fb5b92d9089ec97fa1
Jerry Wang <jerry@jerrywang.website>
2020-06-25 17:15:04 -0700

    Bug fix on unittest

--- test/git.spec.ts
+++ test/git.spec.ts
@@ -2,16 +2,17 @@ import expect from "expect"
 import Git from "nodegit"
 import {tmpdir} from "os"
 import {join} from 'path'
-import {writeFileSync} from "fs";
+import {mkdirSync, writeFileSync,} from "fs";
 import {initRepoAndCommitAll, autoCommitEmail, autoCommitName} from "../src/common";
 
 describe("src/common/git.ts", () => {
-    const testDir = join(tmpdir(), 'test')
+    const testDir = join(tmpdir(), 'test'+Math.random())
+    mkdirSync(testDir)
     it("creates initial commit correctly", async () => {
         writeFileSync(join(testDir, 'test'), 'test git')
         await initRepoAndCommitAll(testDir)
         let repo = await Git.Repository.open(testDir)
-        let commit=await repo.getHeadCommit()
+        let commit = await repo.getHeadCommit()
         expect(commit.author().email()).toBe(autoCommitEmail)
         expect(commit.author().name()).toBe(autoCommitName)
         expect(commit.message()).toBe('Initial commit')
